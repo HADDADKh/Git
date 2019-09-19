fde0ac5c6d9e2d02f117613dccfe2b0ee636578f
Haddad khalifa <haddedkhlifa@gmail.com>
2019-09-19 15:43:35 +0200

    conflut

--- src/BeforeJava8.java
+++ src/BeforeJava8.java
@@ -41,6 +41,7 @@ public class BeforeJava8 {
 	private static Person getStudentByNameeee(List<Person> persons, String name) {
     	Person p1= persons.stream().filter(elemt-> {
     		if (name.equals(elemt.getName()) && 1000==elemt.getAge()) {
+
     			return true;
     		}
     	return false; 
