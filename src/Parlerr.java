c65cd6c3d1240a44d5f0b777e63da1e8557cc9fe
Haddad khalifa <haddedkhlifa@gmail.com>
2019-09-26 14:00:13 +0200

    Interface fonctionnel

---
+++ src/Parlerr.java
@@ -0,0 +1,19 @@
+
+public class Parlerr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		dialogue d=new dialogue() {
+				
+				@Override
+				public void parler(String question) {
+					System.out.println("haddad");			
+				}
+				};
+		d.parler("d");
+		
+
+	}
+
+}
---
+++ src/ParlerrLamdba.java
@@ -0,0 +1,14 @@
+
+public class ParlerrLamdba {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		dialogue d= (a) -> System.out.println("haddad");
+				
+				d.parler("e");
+		
+
+	}
+}
